6d4ccaf0e0d71a03c15790cbc5c73e7934b3e76f
蔡于飛 <54766809+zhmgczh@users.noreply.github.com>
2026-01-20 22:02:51 -0500

    Update.

--- src/main/java/GaussianEliminationSolver.java
+++ src/main/java/GaussianEliminationSolver.java
@@ -1,5 +1,5 @@
 import java.math.BigInteger;
-import java.util.Scanner;
+import java.util.*;
 
 class Rat {
     public static final Rat ZERO = new Rat(BigInteger.ZERO, BigInteger.ONE);
@@ -165,5 +165,134 @@ class GaussianRREF {
 }
 
 public class GaussianEliminationSolver {
+    private static BigInteger[] initialize_coefficients(final int length) {
+        BigInteger[] coefficients = new BigInteger[length];
+        Arrays.fill(coefficients, BigInteger.ZERO);
+        return coefficients;
+    }
 
+    private static HashMap<Integer, Rat> get_unique_solutions(final BigInteger[][] A, final BigInteger[] b) {
+        HashMap<Integer, Rat> unique_solutions = new HashMap<>();
+        Rat[][] rref = GaussianRREF.rrefAugmented(A, b);
+        int current_lower_j = 0;
+        for (Rat[] rats : rref) {
+            for (; current_lower_j < rref[0].length - 1; ++current_lower_j) {
+                if (rats[current_lower_j].isOne()) {
+                    break;
+                }
+            }
+            if (current_lower_j == rref[0].length - 1) {
+                if (rats[current_lower_j].isZero()) {
+                    break;
+                } else {
+                    return null;
+                }
+            }
+            boolean is_unique = true;
+            for (int j = current_lower_j + 1; j < rref[0].length - 1; ++j) {
+                if (!rats[j].isZero()) {
+                    is_unique = false;
+                }
+            }
+            if (is_unique) {
+                unique_solutions.put(current_lower_j, rats[rref[0].length - 1]);
+            }
+            ++current_lower_j;
+        }
+        return unique_solutions;
+    }
+
+    private static void process_number_point(final ArrayList<Pair<Integer, Integer>> block, final char[][] map, final boolean[][] prediction_tag, final Pair<Integer, Integer> number_point, final boolean[][] visited, final HashMap<Pair<Integer, Integer>, Integer> point_index, final ArrayList<BigInteger[]> A, final ArrayList<BigInteger> b) {
+        if (!visited[number_point.getFirst()][number_point.getSecond()]) {
+            int mines = 0;
+            ArrayList<Integer> prediction_point_indices = new ArrayList<>();
+            for (int[] unit_vector : MinesweeperState.unit_vectors) {
+                int new_i = number_point.getFirst() + unit_vector[0];
+                int new_j = number_point.getSecond() + unit_vector[1];
+                if (new_i >= 0 && new_i < map.length && new_j >= 0 && new_j < map[0].length) {
+                    if (prediction_tag[new_i][new_j]) {
+                        prediction_point_indices.add(point_index.get(new Pair<>(new_i, new_j)));
+                    } else if (MinesweeperState.MINE_FLAG == map[new_i][new_j]) {
+                        ++mines;
+                    }
+                }
+            }
+            int total = MinesweeperState.to_number(map[number_point.getFirst()][number_point.getSecond()]) - mines;
+            if (0 == total) {
+                for (int prediction_point_index : prediction_point_indices) {
+                    BigInteger[] coefficients = initialize_coefficients(block.size());
+                    coefficients[prediction_point_index] = BigInteger.ONE;
+                    A.add(coefficients);
+                    b.add(BigInteger.ZERO);
+                }
+            } else if (total == prediction_point_indices.size()) {
+                for (int prediction_point_index : prediction_point_indices) {
+                    BigInteger[] coefficients = initialize_coefficients(block.size());
+                    coefficients[prediction_point_index] = BigInteger.ONE;
+                    A.add(coefficients);
+                    b.add(BigInteger.ONE);
+                }
+            } else {
+                BigInteger[] coefficients = initialize_coefficients(block.size());
+                for (int prediction_point_index : prediction_point_indices) {
+                    coefficients[prediction_point_index] = BigInteger.ONE;
+                }
+                A.add(coefficients);
+                b.add(BigInteger.valueOf(total));
+            }
+            visited[number_point.getFirst()][number_point.getSecond()] = true;
+        }
+    }
+
+    public static Pair<BigInteger[][], BigInteger[]> get_equations_from_block(final ArrayList<Pair<Integer, Integer>> block, final char[][] map, final boolean[][] prediction_tag) {
+        HashMap<Pair<Integer, Integer>, Integer> point_index = new HashMap<>();
+        for (int i = 0; i < block.size(); ++i) {
+            point_index.put(block.get(i), i);
+        }
+        boolean[][] visited = new boolean[map.length][map[0].length];
+        ArrayList<BigInteger[]> A = new ArrayList<>();
+        ArrayList<BigInteger> b = new ArrayList<>();
+        for (Pair<Integer, Integer> point : block) {
+            ArrayList<Pair<Integer, Integer>> numbers_in_domain = MinesweeperState.get_numbers_in_domain(map, point.getFirst(), point.getSecond());
+            for (Pair<Integer, Integer> number_point : numbers_in_domain) {
+                process_number_point(block, map, prediction_tag, number_point, visited, point_index, A, b);
+            }
+        }
+        return new Pair<>(A.toArray(new BigInteger[0][]), b.toArray(new BigInteger[0]));
+    }
+
+    public static ArrayList<Pair<Pair<Integer, Integer>, Character>> get_predictions_from_block(final ArrayList<Pair<Integer, Integer>> block, final char[][] map, final boolean[][] prediction_tag) {
+        ArrayList<Pair<Pair<Integer, Integer>, Character>> predictions = new ArrayList<>();
+        Pair<BigInteger[][], BigInteger[]> equations = get_equations_from_block(block, map, prediction_tag);
+        HashMap<Integer, Rat> unique_solutions = get_unique_solutions(equations.getFirst(), equations.getSecond());
+        if (null == unique_solutions) {
+            return null;
+        }
+        for (int i = 0; i < block.size(); ++i) {
+            if (unique_solutions.containsKey(i)) {
+                Pair<Integer, Integer> point = block.get(i);
+                Rat solution = unique_solutions.get(i);
+                if (solution.isZero()) {
+                    predictions.add(new Pair<>(point, MinesweeperState.ZERO));
+                } else if (solution.isOne()) {
+                    predictions.add(new Pair<>(point, MinesweeperState.ONE));
+                } else {
+                    return null;
+                }
+            }
+        }
+        return predictions;
+    }
+
+    public static ArrayList<Pair<Pair<Integer, Integer>, Character>> get_predictions_from_blocks(final ArrayList<ArrayList<Pair<Integer, Integer>>> blocks, final char[][] map, final boolean[][] prediction_tag) {
+        ArrayList<Pair<Pair<Integer, Integer>, Character>> predictions = new ArrayList<>();
+        for (ArrayList<Pair<Integer, Integer>> block : blocks) {
+            ArrayList<Pair<Pair<Integer, Integer>, Character>> pred = get_predictions_from_block(block, map, prediction_tag);
+            if (null == pred) {
+                return null;
+            }
+            predictions.addAll(pred);
+        }
+        return predictions;
+    }
 }
